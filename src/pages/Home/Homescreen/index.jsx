57d8893ed736dd829c08752077b48448dc7ff141
Sasayi08 <83924324+Sasayi08@users.noreply.github.com>
2024-05-31 10:11:56 +0200

    editted with more images and changed logo

--- src/pages/Home/Homescreen/index.jsx
+++ src/pages/Home/Homescreen/index.jsx
@@ -11,7 +11,7 @@ export default function Home () {
             <MySkills/>
             <AboutMe/>
             <MyPortfolio/>
-            <Testimonial/>
+            {/* <Testimonial/> */}
         </>
     )
 }
--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
       return (
         <nav className={`navbar ${navActive ? "active" : ""}`}>
           <div>
-            <img src="./img/logo.svg" alt="Logoipsum" />
+            <p>Yondela.</p>
           </div>
           <a
             className={`nav__hamburger ${navActive ? "active" : ""}`}
